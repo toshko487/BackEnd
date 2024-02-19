4d3b6a3b8dbe4418a7158c5e821cc50870e60699
toshko487 <toshko487@gmail.com>
2024-02-19 19:21:43 +0800

    up

--- sample-api/main.js
+++ sample-api/main.js
@@ -4,18 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get("/article/create", (req, res) => {
-  const content = [
-    {
-      name: "title",
-      desc: "how touse mark",
-    },
-  ];
-  fs.writeFileSync("articles.json", JSON.stringify(content));
+  const { title, desc } = req.query;
+
+  const data = fs.readFileSync("articles.json", "utf8");
+  const list = JSON.parse(data);
+
+  list.push({
+    title: title,
+    decs: desc,
+  });
+
+  fs.writeFileSync("articles.json", JSON.stringify(list));
   res.send("succes");
 });
 
 app.get("/article/read", (req, res) => {
-  res.send({ name: "hello world" });
+  const data = fs.readFileSync("articles.json", "utf8");
+  res.json(JSON.parse(data));
 });
 
 app.get("/article/update", (req, res) => {
